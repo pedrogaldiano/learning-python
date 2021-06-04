3683b3c9f5a7f476dcc625bf99fea027e30a0f5c
unknown <pedrogaldiano>
2021-06-04 11:54:00 -0300

    Fill all the form before submit and some changes in the indes.html

--- my-projects/workout-creator/application.py
+++ my-projects/workout-creator/application.py
@@ -2,19 +2,23 @@ from flask import Flask, render_template, request
 from create_routine import create_routine
 
 app = Flask(__name__)
+GOALS = ['Gain Muscle', 'Lose Fat', 'Strength']
+
 @app.route("/", methods = ['POST','GET'])
 
 def index():
     if request.method == 'POST':
         user_routine = create_routine(request.form.get('routine'))
-        if not user_routine is None:
-            
-            weight = request.form.get('weight')
-            height = request.form.get('height')
-            return render_template('workout.html', routine=user_routine,
-                                   weight=weight, height=height)
+        goal = request.form.get('goal')
+        weight = request.form.get('weight')
+        height = request.form.get('height')
         
-    return render_template("index.html")
+        validate = [not (x is None or x == '') for x in [user_routine, goal, weight, height]]
+        if all(validate):
+            return render_template('workout.html', routine=user_routine,
+                                    weight=weight, height=height, goal=goal)
+        
+    return render_template("index.html", goals=GOALS)
 
 
 if __name__ == "__main__":  
