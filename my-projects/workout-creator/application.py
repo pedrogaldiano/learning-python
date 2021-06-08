5f4506ced1d00b3130ef93edbb7b7e368d21047e
unknown <pedrogaldiano>
2021-06-07 21:47:49 -0300

    Finished the predefined workout

--- my-projects/workout-creator/application.py
+++ my-projects/workout-creator/application.py
@@ -8,16 +8,25 @@ app = Flask(__name__)
 
 def index():
     if request.method == 'POST':
+        
         if request.form.get('action') == 'Generate Predefined Workout':
+            sets = request.form.get('sets')
+            reps = request.form.get('reps')
+            
             routine_selected = []
             for selected in predefined_routine:
                 if request.form.get(selected) == 'on':
                     routine_selected.append(selected)
                 
                 user_routine = create_basic_routine(routine_selected)
+                routine = zip(routine_selected, user_routine)
+                
+                sets = request.form.get('sets')
+                reps = request.form.get('reps')
 
-            if not user_routine == []:
-                return render_template('workout.html', routine=user_routine)
+            if not (user_routine == [] or sets is None or reps is None):
+                return render_template('workout.html', routine=routine, 
+                                       sets=sets, reps=reps)
             else:
                 pass
         
